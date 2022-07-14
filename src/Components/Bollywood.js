d26e530588125affe00a1d4b4586aac04aac47f7
sameera <48850057+Md-Sameera@users.noreply.github.com>
2022-07-14 20:19:00 +0530

    Update Bollywood.js

--- src/Components/Bollywood.js
+++ src/Components/Bollywood.js
@@ -4,7 +4,7 @@ import "./Styles.css"
 const Bollywood = () => {
     const [data , useData] = useState([])
     useEffect(()=>{
-        const url = "http://localhost:40021/api/bollywood/";
+        const url = "https://reactblogbackend23.herokuapp.com/api/bollywood/";
         fetch(url).then(res => res.json())
         .then(useData)
         .catch(err=>console.log(err))
