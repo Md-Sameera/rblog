f12eea7552e850958a080c20d298d69bc8f5a3c9
sameera <48850057+Md-Sameera@users.noreply.github.com>
2022-07-14 20:21:22 +0530

    Update Technology.js

--- src/Components/Technology.js
+++ src/Components/Technology.js
@@ -4,7 +4,7 @@ import "./Styles.css"
 const Technology=()=> {
   const [data , useData] = useState([])
     useEffect(()=>{
-        const url = "http://localhost:40021/api/technology";
+        const url = "https://reactblogbackend23.herokuapp.com/api/technology";
         fetch(url).then(res => res.json())
         .then(useData)
         .catch(err=>console.log(err))
