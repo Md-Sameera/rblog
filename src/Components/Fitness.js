b71edc174d912955a56438e63a84a29a3b6807f9
sameera <48850057+Md-Sameera@users.noreply.github.com>
2022-07-14 20:19:26 +0530

    Update Fitness.js

--- src/Components/Fitness.js
+++ src/Components/Fitness.js
@@ -4,7 +4,7 @@ import "./Styles.css";
 const Fitness=()=>{
   const [data , useData] = useState([])
     useEffect(()=>{
-        const url = "http://localhost:40021/api/fitness";
+        const url = "https://reactblogbackend23.herokuapp.com//api/fitness";
         fetch(url).then(res => res.json())
         .then(useData)
         .catch(err=>console.log(err))
